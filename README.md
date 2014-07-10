b4937d0bec0af5a08e3c8f94576ecc9f1ea3ff5b
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2014-07-10 21:41:10 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## python-phant
+
+A Python client library for Phant. See
+[data.sparkfun.com](https://data.sparkfun.com/) for more information.
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='phant',
+    version='0.1',
+    author='Matthias Vogelgesang',
+    author_email='matthias.vogelgesang@gmail.com',
+    url='http://github.com/matze/python-phant',
+    py_modules=['phant'],
+    install_requires=['requests'],
+)
